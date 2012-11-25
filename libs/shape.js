1c50c1e22c815e034f2455f0a69bf73a7bd96a93
Dmitrii Samoilov <e2718281828@ya.ru>
2012-11-25 15:41:28 +0200

    added random coordinates when initializing new shapes

--- libs/shape.js
+++ libs/shape.js
@@ -1,9 +1,9 @@
 function Shape(options) {
-	ORIGIN_X = 50.442638;
-	ORIGIN_Y = 30.543365;
+	var ORIGIN_X = 50.442638,
+      ORIGIN_Y = 30.543365;
 
 	var x = options.x || ORIGIN_X, // // instead of latitude and longitude
-			y = options.u || ORIGIN_Y,
+			y = options.y || ORIGIN_Y,
 			world_ref = options['world_ref'],
 			map_ref = options['map_ref'],
 			username = options['name'],
@@ -69,37 +69,10 @@ function Shape(options) {
 	}
   this.draw();
 
-	this.get_random_coordinates = function() {
-		var random_x = ORIGIN_X - 0.02 + (Math.random() / 50.0),
-			random_y = ORIGIN_Y - 0.02 + (Math.random() / 50.0);
-		return random_y + "," + random_x;
-	}
-
   this.get_coordinates = function() {
 	  return [x, y];
   }
   this.get_pixel = function() {
     return element.offset();
   }
-
-	this.get_random_street = function() {
-		var shape = this;
-		jQuery.get('http://geocode-maps.yandex.ru/1.x/?geocode=' + this.get_random_coordinates() +   '&kind=street&spn=0.00005,0.00005&rspn=1&results=1&format=json', function(response) {
-			try {
-				var pos = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"];
-				var point = pos.split(' ');
-				x = parseFloat(point[1]);
-				y = parseFloat(point[0]);
-			} catch(e) {
-				console.log('error in get_random_street');
-				console.log(e);
-				shape.get_random_street();
-			}
-			shape.draw();
-			world_ref.fit_map_to_shapes();
-			return;
-		});
-	}
-
-	//this.get_random_street();
 }
--- server/main.coffee
+++ server/main.coffee
@@ -1,7 +1,31 @@
+ORIGIN_X = 50.442638
+ORIGIN_Y = 30.543365
+	
+get_random_coordinates = ->
+	random_x = ORIGIN_X - 0.02 + (Math.random() / 50.0)
+	random_y = ORIGIN_Y - 0.02 + (Math.random() / 50.0)
+	random_y + "," + random_x
+
+get_random_street = ->
+	shape = this
+	jQuery.get('http://geocode-maps.yandex.ru/1.x/?geocode=' + get_random_coordinates() +   '&kind=street&spn=0.00005,0.00005&rspn=1&results=1&format=json', (response) ->
+		try
+			pos = response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+			point = pos.split(' ')
+			x = parseFloat(point[1])
+			y = parseFloat(point[0])
+			return [x, y]
+		catch e
+			console.log('error in get_random_street')
+			console.log(e)
+			get_random_street()
+	)
+	
 Accounts.onCreateUser (options, user)->
+  coords = get_random_street
   c = Player.insert({
-    x: 50.442638,
-    y: 30.543365,
+    x: coords[0],
+    y: coords[1],
     status: 'alive',
     color: colors[Math.floor(Math.random()*colors.length)],
     kind: kinds[Math.floor(Math.random()*kinds.length)],
